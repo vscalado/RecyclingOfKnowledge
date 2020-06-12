211b6f3814f253be907760d52eea9e61d7874201
vscalado <vitor.santos.calado@gmail.com>
2020-06-12 16:29:31 -0300

    Introdução aos conceitos do Java e Utilização do print

--- Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
+++ Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
@@ -2,6 +2,9 @@ package br.com.treinaweb.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Vitor Calado");        
+        System.out.println("Vitor Calado");     
+        System.out.print("Vitor Calado1");    
+        System.out.println("Vitor Calado2");  
+        System.out.print("Vitor Calado3"); 
     }
 }
