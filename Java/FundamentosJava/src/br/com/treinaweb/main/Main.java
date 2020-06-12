6d19b76ff154547bf015d5ebdf117757c9646bb9
vscalado <vitor.santos.calado@gmail.com>
2020-06-12 17:30:03 -0300

    Declaração de variaveis e leitura de dados do sistema

--- Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
+++ Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
@@ -1,10 +1,19 @@
 package br.com.treinaweb.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Vitor Calado");     
-        System.out.print("Vitor Calado1");    
-        System.out.println("Vitor Calado2");  
-        System.out.print("Vitor Calado3"); 
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        int numero1 = scanner1.nextInt();
+        System.out.println(numero1);
+        System.out.println("Digite qual a operação: ");
+        char operacao = scanner1.next().charAt(0);
+        System.out.println(operacao);
+        System.out.println("Digite o segundo numero: ");
+        int numero2 = scanner1.nextInt();
+        System.out.println(numero2);
+        scanner1.close();
     }
 }
