c581890894ac432099eb87e937c4c48ead64b30d
vscalado <vitor.santos.calado@gmail.com>
2020-06-15 18:06:58 -0300

    Estruturas de tomada de decisão IF

--- Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
+++ Java/FundamentosJava/src/br/com/treinaweb/main/Main.java
@@ -7,13 +7,32 @@ public class Main {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Digite o primeiro numero: ");
         int numero1 = scanner1.nextInt();
-        System.out.println(numero1);
+        //System.out.println(numero1);
+        
         System.out.println("Digite qual a operação: ");
         char operacao = scanner1.next().charAt(0);
-        System.out.println(operacao);
+        //System.out.println(operacao);
+        
         System.out.println("Digite o segundo numero: ");
         int numero2 = scanner1.nextInt();
-        System.out.println(numero2);
+        //System.out.println(numero2);
+        //System.out.println(String.format("Você que fazer a operação %d %c %d", numero1, operacao, numero2));
+
+        int resultado = 0;
+
+        if (operacao == '+') {
+            resultado = numero1 + numero2;
+        }else if (operacao == '-'){
+            resultado = numero1 - numero2;
+        }else if(operacao == 'x'){
+            resultado = numero1 * numero2;
+        }else if(operacao == '/'){
+            resultado = numero1 / numero2;
+        }else {
+            System.out.println("Digite uma operação válida");
+        }
+        System.out.println(String.format("%d %c %d = %d", numero1, operacao, numero2, resultado));
+
         scanner1.close();
     }
 }
